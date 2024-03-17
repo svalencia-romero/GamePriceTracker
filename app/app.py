04eb27c7bf610fe994e90f9ed134dcf80ae9b47c
Santiago Valencia <65482310+Kuja182@users.noreply.github.com>
2024-03-17 22:01:29 +0100

    app funcionan filtros de busqueda, pensando otra forma de poner precios.

--- app/app.py
+++ app/app.py
@@ -26,11 +26,23 @@ st.title("🕹️ Precios de Videojuegos 🎮")
 st.sidebar.markdown(css, unsafe_allow_html=True)
 # st.sidebar.markdown('<p style="font-family: Arial; color: #FFFFFF;font-size: 20px;">Seleccione los filtros necesarios</p>', unsafe_allow_html=True)
 st.sidebar.markdown('<p style="font-family: Arial; color: #FFFFFF;font-size: 20px;">Filtros</p>', unsafe_allow_html=True)
-platform = st.sidebar.selectbox("Plataforma" ,index=None, options=["PS5", "PS4", "No hay información"], key="platform_selectbox",placeholder="Plataforma",label_visibility='collapsed') 
-genre = st.sidebar.selectbox("Género", index=None, options= ['Deporte','Acción','Aventura','Juegos de rol','Conducción/Carreras','Lucha',
+
+platform = st.sidebar.selectbox("Plataforma" ,index=None, options=["Sin filtro","PS5", "PS4", "No hay información"], key="platform_selectbox",placeholder="Plataforma",label_visibility='collapsed') 
+
+genre = st.sidebar.selectbox("Género", index=None, options= ['Sin filtro','Deporte','Acción','Aventura','Juegos de rol','Conducción/Carreras','Lucha',
                                                              'Simulación','Juegos de disparos','Terror','Únicos','Casual','Simulador'
                                                               ,'Arcade','Estrategia','Familia','Grupo','Música/Ritmo','Puzzle','Educativos',
                                                               'Fitness','Agilidad mental','Preguntas y respuestas','Adulto','No hay información'], key="genre_selectbox", placeholder="Género",label_visibility='collapsed')
+
+idioma = st.sidebar.selectbox("Disponibilidad de Idioma", index=None, options= ['No hay información','Coreano','Inglés','Español','Polaco','Alemán'
+                                                                            ,'Ruso','Turco','Francés (Francia)','Árabe','Japonés','Italiano'
+                                                                            ,'Portugués (Brasil)','Español (México)','Portugués (Portugal)'
+                                                                            ,'Chino (Tradicional)','Chino (Simplificado)','Neerlandés','Sueco'
+                                                                            ,'Griego','Checo','Noruego','Finlandés','Húngaro','Danés','Ucraniano'
+                                                                            ,'Francés (Canadá)','Eslovenio','Vietnamita','Tailandés','Croata','Rumano'
+                                                                            ,'Eslovaco','Hindi','Búlgaro','Irlandés','Catalán','Francés (Bélgica)'
+                                                                            ,'Gallego','Vasco','Malayo','Galés','Tagalo','Afrikaans','Gaélico escocés'],key="idioma_selectbox", placeholder="Idioma disponible",label_visibility='collapsed')
+
 st.sidebar.markdown('<p style="font-family: Arial;color: #FFFFFF;font-size: 20px;">Rango de Precio</p>', unsafe_allow_html=True)
 price_range = st.sidebar.slider("Precios",0,100,(0,60),key="price_range_slider",label_visibility='collapsed')
 
@@ -39,10 +51,11 @@ st.dataframe(prueba_df)
 
 # Filtrar datos según la selección del usuario
 filtered_data = prueba_df[
-    (prueba_df["Plataforma"] == platform) |
-    (platform == "No hay información")
-    & (prueba_df["Genero"] == genre)
-    & (prueba_df["Precio actual con PSN"].between(price_range[0], price_range[1]))
+    (prueba_df["Plataforma"].str.contains(fr'\b{platform}\b', na=False)) &
+    (prueba_df["Genero"] == genre) &
+    (prueba_df["Precio actual con PSN"].between(price_range[0], price_range[1])) &
+    (prueba_df["Idiomas"].str.contains(fr'\b{idioma}\b', na=False))
+
 ]
 
 # Mostrar resultados
